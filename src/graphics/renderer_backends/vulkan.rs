6491e92acc490a9f8acfe2791f3d3c86fae72eab
N-Adkins <N-Adkins@github.com>
2024-08-22 17:54:03 -0500

    Work on vulkan

--- src/graphics/renderer_backends/vulkan.rs
+++ src/graphics/renderer_backends/vulkan.rs
@@ -4,26 +4,35 @@ use ash::{vk, Entry, Instance};
 use crate::graphics::Renderer;
 
 /// This literally only exists to implement Debug on ash::Entry.
-struct EntryWrapper {
-    entry: Entry,
-}
+struct EntryWrapper(Entry);
 
 impl std::fmt::Debug for EntryWrapper {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "")
+        write!(f, "Vulkan Entry")
+    }
+}
+
+struct InstanceWrapper(Instance);
+
+impl std::fmt::Debug for InstanceWrapper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Vulkan Instance")
     }
 }
 
 #[derive(Debug)]
 pub struct VulkanRenderer {
     entry: EntryWrapper, // Basically the vulkan context.
+    instance: InstanceWrapper,
 }
 
 impl VulkanRenderer {
     pub fn new() -> Result<Self> {
         let entry = unsafe { Entry::load()? };
+        let instance = Self::create_instance(&entry)?;
         let this = Self {
-            entry: EntryWrapper { entry },
+            entry: EntryWrapper(entry),
+            instance: InstanceWrapper(instance),
         };
         Ok(this)
     }
