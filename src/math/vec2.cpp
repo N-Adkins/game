206f198daa71962fabe3d09f3eeeabbec474c8ea
N-Adkins <N-Adkins@github.com>
2024-09-16 16:48:50 -0500

    added vec

---
+++ src/math/vec2.cpp
@@ -0,0 +1,69 @@
+#include "vec2.hpp"
+
+namespace Engine {
+
+float Vec2::getX()
+{
+    return x;
+}
+
+float Vec2::getY()
+{
+    return y;
+}
+
+void Vec2::setX(float x)
+{
+    this->x = x;
+}
+
+void Vec2::setY(float y)
+{
+    this->y = y;
+}
+
+Vec2 Vec2::operator+(const Vec2& rhs)
+{
+    return Vec2(x + rhs.x, y + rhs.y);
+}
+
+void Vec2::operator+=(const Vec2& rhs)
+{
+    x += rhs.x;
+    y += rhs.y;
+}
+
+Vec2 Vec2::operator-(const Vec2& rhs)
+{
+    return Vec2(x - rhs.x, y - rhs.y);
+}
+
+void Vec2::operator-=(const Vec2& rhs)
+{
+    x -= rhs.x;
+    y -= rhs.y;
+}
+
+Vec2 Vec2::operator*(float rhs)
+{
+    return Vec2(x * rhs, y * rhs);
+}
+
+void Vec2::operator*=(float rhs)
+{
+    x *= rhs;
+    y *= rhs;
+}
+
+Vec2 Vec2::operator/(float rhs)
+{
+    return Vec2(x / rhs, y / rhs);
+}
+
+void Vec2::operator/=(float rhs)
+{
+    x /= rhs;
+    y /= rhs;
+}
+
+} // namespace Engine
--- src/math/vec2.hpp
+++ src/math/vec2.hpp
@@ -8,6 +8,21 @@ public:
     Vec2(float x, float y)
         : x(x), y(y) {}
 
+    float getX();
+    float getY();
+    void setX(float x);
+    void setY(float y);
+
+    Vec2 operator+(const Vec2& rhs);
+    void operator+=(const Vec2& rhs);
+    Vec2 operator-(const Vec2& rhs);
+    void operator-=(const Vec2& rhs);
+    Vec2 operator*(float rhs);
+    void operator*=(float rhs);
+    Vec2 operator/(float rhs);
+    void operator/=(float rhs);
+    void operator,(float rhs);
+
 private:
     float x = 0.f;
     float y = 0.f;
