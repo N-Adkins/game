7cd1be095219c8f7a21718647f9c1ff397a64b17
N-Adkins <noahcadkins@gmail.com>
2024-10-09 18:07:30 -0400

    Correct deltatime

--- resources/test.lua
+++ resources/test.lua
@@ -1,17 +1,14 @@
 local script = {}
-
 local Engine = require("Engine")
-
 Engine.SetVSync(false)
-
 local sprite = Engine.CreateSprite()
 
 -- Variables for velocity and acceleration
 local velocity = Vec2.new(0, 0)
 local acceleration = Vec2.new(0, 0)
-local maxSpeed = 10
-local accelRate = 1 -- Rate at which acceleration increases
-local friction = 0.999   -- Friction for slowing down the sprite
+local maxSpeed = 5000
+local accelRate = 40000000 -- Increased to compensate for delta time
+local friction = 100 -- Adjusted for more appropriate friction
 
 function script:OnFrame(delta_time)
     -- Reset acceleration each frame
@@ -36,8 +33,12 @@ function script:OnFrame(delta_time)
         acceleration = acceleration / acceleration:Length() * accelRate
     end
 
+    -- Apply friction
+    local friction_force = velocity * -1 * friction
+    acceleration = acceleration + friction_force
+
     -- Update velocity based on acceleration
-    velocity = velocity + acceleration
+    velocity = velocity + acceleration * delta_time
 
     -- Clamp velocity to maxSpeed
     if velocity:Length() > maxSpeed then
@@ -45,12 +46,12 @@ function script:OnFrame(delta_time)
     end
 
     -- Update sprite position based on velocity
-    sprite.position = sprite.position + velocity * delta_time * 100
+    sprite.position = sprite.position + velocity * delta_time
 
-    --sprite.scale = 1 - (math.abs(velocity.x) + math.abs(velocity.y)) / 2;
-
-    -- Apply friction to slow down the sprite
-    velocity = velocity * friction
+    -- Debug output
+    print("Delta time: " .. delta_time)
+    print("Acceleration: " .. tostring(acceleration))
+    print("Velocity: " .. velocity:Length() .. ", Position: " .. tostring(sprite.position))
 end
 
 return script
